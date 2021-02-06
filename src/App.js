f3208cfe1717c96f19d2782601ff8f9aaa074cac
Orwa_Watad <theorwa@gmail.com>
2021-02-06 15:18:37 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,61 +1,28 @@
-import logo from './logo.svg';
-import loading from './loading.gif';
-import './App.css';
+import Login from "./containers/login/Login";
+import Overview from "./containers/overview/Overview";
 import {useEffect, useState} from 'react';
-import axios from 'axios';
 
 const App = ({
 
 }) => {
-  const [data, setdata] = useState("");
-  const [timers, settimers] = useState("");
-  const [temp, settemp] = useState("");
+    const [login, setlogin] = useState(undefined);
 
-  useEffect(() => {
-    data || getData();
-  }, [data]);
+    useEffect(() => {
+        lastLogin();
+      }, [login]);
 
-  const getData = () => {
-    axios.get('https://timers-comp-heater.herokuapp.com/data')
-    .then(res => {
-      console.log(res);
-      settimers(res.data.timers);
-      settemp(res.data.temp);
-    })
-  };
+    const lastLogin = () => {
+        const username = localStorage.getItem('username');
+        const password = localStorage.getItem('password');
+        setlogin(username && password && username === "admin" && password === "admin");
+    }
 
-  function NewlineText(props) {
-    const text = props.text;
-    return text.split(',').map(str => 
-      <tr>
-        <td>{str.split('=')[0]}:</td>
-        <td>{str.split('=')[1]}</td>
-      </tr>
+    return (
+        <div>
+            { login === undefined ? (null) : (!login ? (<Login setlogin={setlogin}/>) : (<Overview setlogin={setlogin}/>)) }
+        </div>
     );
-  }
-
-  return (
-    <div className="App">
-      <header className="App-header">
-
-        { (!timers || !temp) ? (null) // (<img className='loading' src={loading} alt="Loading" width="200" height="200" />) 
-        : (<div className="App-header">
-          <h1>Timers</h1>
-          <div>
-            <NewlineText text={timers} />
-          </div>
-
-          <br/>
-
-          <h1>Temperature</h1>
-          <div>
-            <NewlineText text={temp} />
-          </div>
-        </div>) }
-
-      </header>
-    </div>
-  );
 }
-
+    
 export default App;
+    
